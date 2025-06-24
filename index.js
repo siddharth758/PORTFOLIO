d12b475a3143fa74159347e9d9f30da781dc3a71
Siddharth Jha <notsiddharth962@gmail.com>
2025-06-24 07:17:24 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// var tabLinks = document.getElementsByClassName("tab-links");
+// var tabContents = document.getElementsByClassName("tab-contents");
+
+// function opentab(tabname) {
+//   for (let tablink of tabLinks) {
+//     tablink.classList.remove("active-links");
+//   }
+//   for (let tabcontent of tabContents) {
+//     tabcontent.classList.remove("active-tabs");
+//   }
+//   event.currentTarget.classList.add("active-links");
+//   document.getElementById(tabname).classList.add("active-tabs");
+// }
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (let tablink of tabLinks) {
+    tablink.classList.remove("active-links");
+  }
+  for (let tabcontent of tabContents) {
+    tabcontent.classList.remove("active-tabs");
+  }
+  event.currentTarget.classList.add("active-links");
+  let selectedTabs = document.getElementsByClassName(tabname);
+  for (let tab of selectedTabs) {
+    tab.classList.add("active-tabs");
+  }
+}
