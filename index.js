d7d4cfed74529914d315ab3c27d78e7c3ae0fa20
Siddharth Jha <notsiddharth962@gmail.com>
2025-08-21 10:42:33 +0530

    Updated portfolio files and added images

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// var tabLinks = document.getElementsByClassName("tab-links");
-// var tabContents = document.getElementsByClassName("tab-contents");
-
-// function opentab(tabname) {
-//   for (let tablink of tabLinks) {
-//     tablink.classList.remove("active-links");
-//   }
-//   for (let tabcontent of tabContents) {
-//     tabcontent.classList.remove("active-tabs");
-//   }
-//   event.currentTarget.classList.add("active-links");
-//   document.getElementById(tabname).classList.add("active-tabs");
-// }
 var tabLinks = document.getElementsByClassName("tab-links");
 var tabContents = document.getElementsByClassName("tab-contents");
 
