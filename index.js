f911f0f8cd588977a7eaacb9af27b0bb7275092e
Siddharth Jha <notsiddharth962@gmail.com>
2025-11-11 14:29:33 +0530

    Updated portfolio files and added new project images

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var tabLinks = document.getElementsByClassName("tab-links");
 var tabContents = document.getElementsByClassName("tab-contents");
 
-function opentab(tabname) {
+function opentab(tabname, event) {
   for (let tablink of tabLinks) {
     tablink.classList.remove("active-links");
   }
@@ -9,8 +9,5 @@ function opentab(tabname) {
     tabcontent.classList.remove("active-tabs");
   }
   event.currentTarget.classList.add("active-links");
-  let selectedTabs = document.getElementsByClassName(tabname);
-  for (let tab of selectedTabs) {
-    tab.classList.add("active-tabs");
-  }
+  document.getElementById(tabname).classList.add("active-tabs");
 }
